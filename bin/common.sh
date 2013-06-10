bfc810b11add65c1c6c313ba4b9933d1f657bb06
Tom Carchrae <tom@intellecti.ca>
2013-06-10 15:00:41 -0700

    allow version with git hash, point to github to download dist, use zip

--- bin/common.sh
+++ bin/common.sh
@@ -8,7 +8,7 @@ get_play_version()
     return 0
   fi
 
-  grep -P '.*-.*play[ \t]+[0-9\.]' ${file} | sed -E -e 's/[ \t]*-[ \t]*play[ \t]+([0-9A-Za-z\.]*).*/\1/'    
+  grep -P '.*-.*play[ \t]+[0-9\.]' ${file} | sed -E -e 's/[ \t]*-[ \t]*play[ \t]+([0-9A-Za-z\.]*)/\1/'    
 }
 
 check_compile_status()
@@ -24,7 +24,10 @@ check_compile_status()
 install_play()
 {
   VER_TO_INSTALL=$1
-  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+
+#  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  PLAY_URL="https://github.com/intellectica/play-1.3.x-distribution/raw/master/play-$VER_TO_INSTALL.zip"
+
   PLAY_TAR_FILE="play-heroku.tar.gz"
   echo "-----> Installing Play! $VER_TO_INSTALL....."
   curl --silent --max-time 150 --location $PLAY_URL -o $PLAY_TAR_FILE
@@ -36,8 +39,10 @@ install_play()
     echo "-----> Error installing Play! framework or unsupported Play! framework version specified. Please review Dev Center for a list of supported versions."
     exit 1
   fi
-  tar xzf $PLAY_TAR_FILE
+#  tar xzf $PLAY_TAR_FILE
+  unzip $PLAY_TAR_FILE
   rm $PLAY_TAR_FILE
   chmod +x $PLAY_PATH/play
   echo "-----> done"
 }
+
